627c212d007e1e31503989e6efc7d42c4828c34b
Bryan Kizer <brykizer@gmail.com>
2013-06-15 18:41:14 -0400

    added treeview, really just changed its scrollviewer

--- build.bat
+++ build.bat
@@ -5,8 +5,8 @@ REM Generate SandCastle Documentation
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "E:\Programming\!Csharp\Metro UI Toolkit\Metro-UI-Toolkit\MutDocs\MutDocs.shfbproj"
 
 REM Clean up
-COPY "E:\Programming\!Csharp\Metro UI Toolkit\Metro-UI-Toolkit\MutDocs\Presentation.css" "E:\Programming\!Csharp\Metro UI Toolkit\Metro-UI-Toolkit\MutDocs\Help\styles"
-COPY "E:\Programming\!Csharp\Metro UI Toolkit\Metro-UI-Toolkit\MutDocs\TOC.css" "E:\Programming\!Csharp\Metro UI Toolkit\Metro-UI-Toolkit\MutDocs\Help"
+COPY "E:\Programming\!Csharp\Presentation.css" "E:\Programming\!Csharp\Metro UI Toolkit\Metro-UI-Toolkit\MutDocs\Help\styles"
+COPY "E:\Programming\!Csharp\TOC.css" "E:\Programming\!Csharp\Metro UI Toolkit\Metro-UI-Toolkit\MutDocs\Help"
 "C:\Program Files\7-Zip\7z.exe" a -mx9 "E:\Programming\!Csharp\Metro UI Toolkit\Metro-UI-Toolkit\MutDocs.7z" "E:\Programming\!Csharp\Metro UI Toolkit\Metro-UI-Toolkit\MutDocs\Help"
 
 REM Open the generate docs
